0a9796241a06a84192184d68ae043e29293c00b7
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-02 02:35:03 +0300

    refactor weather context --> handle loading and weather data in component

--- src/context/WeatherContext.tsx
+++ src/context/WeatherContext.tsx
@@ -4,10 +4,6 @@ import { CityOption } from "../components/CityAutocomplete.tsx";
 interface IWeatherContext {
   selectedCity: CityOption | null;
   setSelectedCity: React.Dispatch<React.SetStateAction<CityOption | null>>;
-  loading: boolean;
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  weatherData: null;
-  setWeatherData: React.Dispatch<React.SetStateAction<null>>;
 }
 
 const WeatherContext = createContext<IWeatherContext | null>(null);
@@ -18,16 +14,10 @@ export const WeatherProvider = ({
   children: React.ReactNode;
 }) => {
   const [selectedCity, setSelectedCity] = useState<CityOption | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [weatherData, setWeatherData] = useState(null);
 
   const initialValue = {
     selectedCity,
     setSelectedCity,
-    loading,
-    setLoading,
-    weatherData,
-    setWeatherData,
   };
 
   return (
