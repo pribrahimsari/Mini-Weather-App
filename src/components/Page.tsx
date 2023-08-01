dda444d2d7bbbc2dee0d7d820dfc08de69d0d889
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-02 00:59:36 +0300

    skeleton for the desired design with

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,33 @@
+import { Box, Paper } from "@mui/material";
+import { SxProps } from "@mui/system";
+import { Theme } from "@mui/material/styles";
+import CityAutocomplete from "./CityAutocomplete.tsx";
+
+const paperSxProps: SxProps<Theme> = {
+  width: "50vw",
+  minHeight: "20vh",
+  padding: 2,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+  gap: 2,
+};
+
+const cityAutocompleteWrapperSxProps: SxProps<Theme> = {
+  minWidth: "15rem",
+};
+
 const Page = () => {
-  return <div>{/* todo */}</div>;
+  return (
+    <Paper sx={paperSxProps} elevation={3} square={false}>
+      <Box sx={cityAutocompleteWrapperSxProps}>
+        <CityAutocomplete />
+      </Box>
+
+      <Box>todo</Box>
+    </Paper>
+  );
 };
 
 export default Page;
