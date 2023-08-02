483278acf85e36dbb7f50bfc928a2e154fe7f60e
pribrahimsari <pr.ibrahimsari@gmail.com>
2023-08-02 23:32:13 +0300

    improve skeleton loading component

--- src/components/SkeletonLoading.tsx
+++ src/components/SkeletonLoading.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Box, Skeleton, Stack } from "@mui/material";
+
+const SingleSkeleton = () => {
+  return (
+    <Stack
+      spacing={1}
+      sx={{
+        minWidth: 90,
+      }}
+    >
+      <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+      <Skeleton variant="circular" width={40} height={40} />
+      <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+    </Stack>
+  );
+};
 
 const SkeletonLoading = () => {
-  return <div>TODO: Loading</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      {[1, 2, 3, 4, 5].map(i => (
+        <SingleSkeleton key={i} />
+      ))}
+    </Box>
+  );
 };
 
 export default SkeletonLoading;
